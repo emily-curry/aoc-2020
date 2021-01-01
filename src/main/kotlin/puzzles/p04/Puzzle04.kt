6891c29948184981b17cfa7f8eee471e983d1922
Emily Curry <emily@curry.sh>
2021-01-01 16:38:39 -0600

    chore: run configs

---
+++ src/main/kotlin/puzzles/p04/Puzzle04.kt
@@ -0,0 +1,12 @@
+package puzzles.p04
+
+import lib.input.InputReader
+import lib.runner.PuzzleId
+import lib.runner.PuzzleRunner
+
+@PuzzleId("04")
+class Puzzle04(input: InputReader) : PuzzleRunner(input) {
+    override fun run() {
+        TODO("Not yet implemented")
+    }
+}
