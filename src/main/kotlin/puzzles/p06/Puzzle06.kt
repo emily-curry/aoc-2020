d2da3c571ff3fe666e2772cece403d9828684c21
Emily Curry <emily@curry.sh>
2021-01-04 18:23:45 -0600

    Puzzle 06 boilerplate

---
+++ src/main/kotlin/puzzles/p06/Puzzle06.kt
@@ -0,0 +1,12 @@
+package puzzles.p06
+
+import lib.input.InputReader
+import lib.runner.PuzzleId
+import lib.runner.PuzzleRunner
+
+@PuzzleId("06")
+class Puzzle06(input: InputReader) : PuzzleRunner(input) {
+    override fun run() {
+        TODO("Not yet implemented")
+    }
+}
