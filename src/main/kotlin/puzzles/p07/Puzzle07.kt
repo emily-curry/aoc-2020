140fa2f437d1a968a87327ce7dd710e5f6954f72
Emily Curry <emily@curry.sh>
2021-01-12 20:30:56 -0600

    puzzle 07 boilerplate

---
+++ src/main/kotlin/puzzles/p07/Puzzle07.kt
@@ -0,0 +1,11 @@
+package puzzles.p07
+
+import lib.input.InputReader
+import lib.runner.PuzzleId
+import lib.runner.PuzzleRunner
+
+@PuzzleId("07")
+class Puzzle07(input: InputReader) : PuzzleRunner(input) {
+    override fun run() {
+    }
+}
